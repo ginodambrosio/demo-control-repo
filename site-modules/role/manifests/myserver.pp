0174c356ac11aa1b056a1243d1c185f9241fa99e
Gino DAmbrosio <gino.dambrosio@puppet.com>
2020-01-06 13:02:24 +1100

    Change 0

--- site-modules/role/manifests/myserver.pp
+++ site-modules/role/manifests/myserver.pp
@@ -1,6 +1,5 @@
 # This role would be made of all the profiles that need to be included to make a myserver work
 # All roles should include the base profile
-#
 class role::myserver {
   include profile::base
 }
