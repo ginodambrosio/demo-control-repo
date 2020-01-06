c11a0751d9ee8fea7907a7ed4abaded4f19b5a46
Gino DAmbrosio <gino.dambrosio@puppet.com>
2020-01-06 14:48:19 +1100

    Add ngrok tunnel for github to CD4PE

--- site-modules/profile/manifests/linux/github_tunnel.pp
+++ site-modules/profile/manifests/linux/github_tunnel.pp
@@ -1,8 +1,10 @@
 # Uses the gabe-ngrok forge module to make a tunnel for CD4PE webhook.
-class { 'ngrok':
-  authtoken => '84fAzMWrconh6dpfd1kRb_3DBPbRkWkNPYjiBNQUYYQ',
-}
-ngrok::tunnel { 'cd4pe':
-  proto => 'tcp',
-  addr  => '8000',
+class profile::linux::github_tunnel {
+  class { 'ngrok':
+    authtoken => '84fAzMWrconh6dpfd1kRb_3DBPbRkWkNPYjiBNQUYYQ',
+  }
+  ngrok::tunnel { 'cd4pe':
+    proto => 'tcp',
+    addr  => '8000',
+  }
 }
